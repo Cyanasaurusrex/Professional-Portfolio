3e1b30652f5f9bce3e03766969d3fef4ee97d20c
Nathan Moon <moonman700@gmail.com>
2023-05-09 18:07:26 -0400

    updated portfolio to include Project 2

---
+++ index.js
@@ -0,0 +1,10 @@
+const topImg = document.querySelector('.top_img')
+
+
+window.addEventListener("load", () => {
+    
+    topImg.addEventListener("click", async () => {
+        window.location.href = "https://www.putasporkinit.co/"
+    })
+    
+})
